842118586e07ac775ad18a781f6e162339e3648e
kowalskr <kowalskr@rose-hulman.edu>
2017-11-27 21:53:27 -0500

    Put name on work.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,11 +2,11 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Kenny Kowalski.
 """
 ########################################################################
 # DONE: 1.
-# On Line 5 above, replace  Kenny Kowalski  with your own name.
+# On Line 5 above, replace  PUT_YOUR-NAME-HERE  with your own name.
 ########################################################################
 
 ########################################################################
