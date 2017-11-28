5fb413e152ccfd2862ff189c4cafe97d4a822ae6
kowalskr <kowalskr@rose-hulman.edu>
2017-11-27 21:51:42 -0500

    Finished all of the TODOs in m3_turtles

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -5,12 +5,12 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
          their colleagues and PUT_YOUR_NAME_HERE.
 """
 ########################################################################
-# TODO: 1.
-# On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
+# DONE: 1.
+# On Line 5 above, replace  Kenny Kowalski  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -27,4 +27,45 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #  fixing them at either this session OR at the NEXT session.
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
-########################################################################
+##########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+mr_red = rg.SimpleTurtle('turtle')
+mr_red.pen = rg.Pen('red','3')
+mr_red.speed = 150
+
+mr_blue = rg.SimpleTurtle('turtle')
+mr_blue.pen = rg.Pen('blue','10')
+mr_blue.speed = 150
+
+mr_red.pen_up()
+mr_red.right(135)
+mr_red.forward(200)
+mr_red.left(135)
+mr_red.pen_down()
+
+size = 300
+
+for k in range(24):
+
+    mr_blue.draw_circle(100)
+    mr_blue.pen_up()
+    mr_blue.right(15)
+    mr_blue.pen_down()
+
+    mr_red.draw_square(size)
+    mr_red.pen_up()
+    mr_red.right(135)
+    mr_red.forward(20)
+    mr_red.left(135)
+    mr_red.pen_down()
+
+    size = size + 30
+
+
+window.close_on_mouse_click()
+
+
